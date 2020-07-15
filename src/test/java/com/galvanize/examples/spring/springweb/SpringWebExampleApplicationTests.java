76a922721d870d817086f1bd3ceff381586d93fc
Richard Key <rich@busyrich.com>
2020-07-15 13:20:44 -0700

    init commit

---
+++ src/test/java/com/galvanize/examples/spring/springweb/SpringWebExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.examples.spring.springweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
